e0038464da354cc5ab9167f86ce93d1c0abdd27c
岳宁 <@suyuening>
2015-11-05 23:03:15 +0800

    添加字符串练习文件

---
+++ com/suyuening/python/chapter2/Tuple.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+'''
+Created on 2015年11月2日
+
+@author: 岳宁
+'''
+print 1, 2, 3
+print (1, 2, 3)
+print 42,
+print 3 * (40 + 2)
+print 3 * (40, +2)
+print 3 * (40 + 2,)
---
+++ com/suyuening/python/chapter3/String.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+'''
+Created on 2015年11月2日
+
+@author: 岳宁
+'''
+
+format1 = "Hello, %s. %s enough for ya?"
+values = ('world', 'Hot')
+print format1 % values
+
+format2 = "Pi with three decimals:%.3f"
+from math import pi
+print format2 % pi
+print pi
+
+from string import Template
+s = Template('$x, glorious $x!')
+print s.substitute(x='slum')
+
+s = Template("It's ${x}tastic!")
+print s.substitute(x='slum')
+
+s = Template("Make $$ selling $x!")
+print s.substitute(x='slum')
